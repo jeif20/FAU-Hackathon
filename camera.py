19887ef6407543b8fcee8ff495e4720f1e5c69f7
Juan Andonaire <jandonairevi2015@fau.edu>
2018-02-20 19:02:51 -0500

    Raspberry Camera

---
+++ camera.py
@@ -0,0 +1,8 @@
+from picamera import PiCamera
+from time import sleep
+
+camera = PiCamera()
+        camera.start_preview()
+        sleep(3)
+        camera.capture('/home/pi/picture.jpg')
+        camera.stop_preview()
